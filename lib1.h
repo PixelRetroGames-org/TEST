83366e7113b6d5d778c773c25df447c3e632b6e5
PRG <pixelretrogames@github.com>
2016-02-08 11:50:22 +0200

    TEST

---
+++ lib1.h
@@ -0,0 +1,6 @@
+#include<cstdio>
+#include "lib.h"
+void aduna()
+{
+ n++;
+}
---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include<cstdio>
+#include "lib1.h"
+int n;
+int main()
+{
+ scanf("%d ",&n);
+ aduna();
+ printf("%d ",n);
+ return 0;
+}
